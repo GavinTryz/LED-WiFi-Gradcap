f95f97091267bf945e9f5d2fe37d215dc12d1bd6
GavinTryz <gavintryz@gmail.com>
2022-05-06 00:50:56 -0400

    Reverted, going to use git --skip-worktree

--- src/Secret.h
+++ src/Secret.h
@@ -1,2 +1,2 @@
-#define AP_SSID "??this is??"
-#define AP_PASS "??a test2??"
+#define AP_SSID "????????" // Enter your desired access point SSID
+#define AP_PASS "????????" // Enter your desired access point password (at least 8 characters)
